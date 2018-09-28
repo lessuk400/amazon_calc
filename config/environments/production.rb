ab6b2a5918b88e2d3cccd2091d00eb9410224cb9
lessuk400 <alex.ostashchenko@gmail.com>
2018-09-28 11:47:45 +0300

    Deploy issue (#5)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,36 +4,34 @@ Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
 
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local               = false
+  config.action_controller.perform_caching         = true
 
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled                = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor                      = Uglifier.new(harmony: true)
 
-  config.assets.compile = false
+  config.assets.compile                            = false
 
-  config.active_storage.service = :local
+  config.active_storage.service                    = :local
 
-  config.log_level = :debug
+  config.log_level                                 = :debug
 
-  config.log_tags = [:request_id]
+  config.log_tags                                  = [:request_id]
 
+  config.action_mailer.perform_caching             = false
 
-  config.action_mailer.perform_caching = false
+  config.i18n.fallbacks                            = true
 
-  config.i18n.fallbacks = true
+  config.active_support.deprecation                = :notify
 
-  config.active_support.deprecation = :notify
+  config.log_formatter                             = ::Logger::Formatter.new
 
-  config.log_formatter = ::Logger::Formatter.new
-
-
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled                = ENV['RAILS_SERVE_STATIC_FILES'].present?
   if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
+    logger                                         = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter                               = config.log_formatter
+    config.logger                                  = ActiveSupport::TaggedLogging.new(logger)
   end
 
   config.active_record.dump_schema_after_migration = false
